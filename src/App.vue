c55a1c1ba30fd289a667af7b505066cccf4b88fa
zff <2312822746@qq.com>
2019-08-06 20:58:54 +0800

    init my project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <block></block>
+  </div>
+</template>
+
+<script>
+import Block from '@/components/Block.vue'
+
+export default {
+  name: "App",
+  components:{
+    Block
+  }
+};
+</script>
+
+<style lang="less">
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+// 导入  MUI 的样式
+import '@/assets/mui/css/mui.min.css'
+
+
+
+// 按需导入 Mint-UI 中的组件
+import 'mint-ui/lib/style.css'
+import {Header} from 'mint-ui'
+Vue.component(Header.name, Header)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
